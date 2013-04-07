87b69c66c48cd591fd6d315ec293781806748f82
Craig Roberts <craig0990@googlemail.com>
2013-04-07 19:10:32 +0100

    initial logged in users

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ server.listen(3000);
 // Configuration
 app.configure(function(){
   app.set("view options", {layout: false});
-  app.use(express.cookieParser('fhJY382YDAWXI'));
+  app.use(express.cookieParser());
   app.use(express.session({cookie: { path: '/', httpOnly: true, expires: false}, secret: 'fhJY382YDAWXI'}));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -48,80 +48,92 @@ io.sockets.on('connection', function (socket) {
   socket.on('startQuiz', function (data) {
     quizMaster.beginQuiz();
   });
+
+  socket.username = false;
+
+  // Listen for new users on frontend
+  socket.on('newUser', function(data) {
+    quizMaster.login(socket, data.username);
+  });
+
+  socket.on('disconnect', function() {
+    console.error('discconnnn', this.username);
+    quizMaster.logout(this.username);
+  });
+
+  socket.on('loginPlayer', function(data) {
+    var error = false;
+
+    // Check to see if a user already exists
+    database.readUsers({
+      username: data.username
+    }, function(err, users) {
+      if (err) {
+        error = err;
+      } else {
+        // Check if user exists, if not, create it
+        if (users.length === 0) {
+          // New user logging in
+          database.createUser({
+            username: data.username
+          }, function(err) {
+            if (err) {
+              error = err;
+            } else {
+              console.log('Created and logged in user', data.username);
+            }
+          });
+        } else {
+          console.log('Logged in user', data.username);
+        }
+
+        if (!error) {
+          quizMaster.login(socket, data.username);
+        }
+
+        socket.emit('loginPlayerResponse', {success: (error === false), error: error});
+      }
+    });
+  });
+
+  socket.on('loginAdmin', function(data) {
+    var error = false;
+
+    database.readUsers({
+      username: 'admin',
+      password: req.body.password
+    }, function(err, users) {
+      if (err) {
+        error = err;
+      } else {
+        if (users.length === 0) {
+          error = 'Password does not match';
+        } else {
+          console.log('Admin logged in');
+          // req.session.username = 'admin';
+          // Hash the username and password as a pseudo-secure admin token
+          // var sha = crypto.createHash('sha1');
+          // sha.update(req.body.username + req.body.password);
+          // req.session.admin = sha.digest('hex');
+        }
+      }
+
+      socket.emit('loginAdminResponse', {success: (error === false), error: error, session: req.session});
+    });
+  });
+
 });
 
 // Quiz: Begin
 //quizMaster.beginQuiz();
     
-// HTTP Routes
-app.post('/loginAdmin', function(req, res) {
-  var error = false;
-
-  database.readUsers({
-    username: 'admin',
-    password: req.body.password
-  }, function(err, users) {
-    if (err) {
-      error = err;
-    } else {
-      if (users.length === 0) {
-        error = 'Password does not match';
-      } else {
-        console.log('Admin logged in');
-        req.session.username = 'admin';
-        // Hash the username and password as a pseudo-secure admin token
-        var sha = crypto.createHash('sha1');
-        sha.update(req.body.username + req.body.password);
-        req.session.admin = sha.digest('hex');
-      }
-    }
-
-    res.send({success: (error === false), error: error, session: req.session});
-  });
+// HTTP GET Routes
+app.get('/loggedIn', function(req, res) {
+  res.send(quizMaster.loggedIn());
 });
 
-app.post('/loginPlayer', function(req, res) {
+// HTTP POST Routes
 
-  var error = false;
-
-  // Check to see if a user already exists
-  database.readUsers({
-    username: req.body.username
-  }, function(err, users) {
-    if (err) {
-      error = err;
-    } else {
-      // Check if user exists, if not, create it
-      if (users.length === 0) {
-        // New user logging in
-        database.createUser({
-          username: req.body.username
-        }, function(err) {
-          if (err) {
-            error = err;
-          } else {
-            console.log('Created and logged in user', req.body.username);
-          }
-        });
-      } else {
-        console.log('Logged in user', req.body.username);
-      }
-
-      // Either way, set a username session key
-      req.session.username = req.body.username;
-      res.send({success: (error === false), error: error, session: req.session});
-
-      // Inform connected clients
-      io.sockets.emit('newUser', {
-          username: req.body.username
-        });
-
-    }
-  });
-
-});
-
-// HTTP API Routes
 app.post('/answer', function(req, res) {
   var error = false;
   if (!req.session.username) {
